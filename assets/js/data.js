a7ab6ceb92ee70cc913e7b3ae2694d1b7c32ca65
Brian2703 <brianong470@gmail.com>
2025-02-03 21:50:14 +1100

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,72 @@
+const projects = [
+    {
+        title: "Payroll Remediation & Employee Dashboard",
+        brief: "Cleaning and modifying the dataset for date-time and numeric values to ensure data accuracy, then extracting and verifying employee payments during leave periods to correct scheduled payment and developed an interactive payroll dashboard in Tableau.",
+        tools: ["Tableau", "SQL"],
+        link: "https://github.com/Brian2703/Payroll_Remediation_and_Employee_Dashboard",
+        image: "images/projects/Payroll Remediation & Employee Dashboard.png"
+    },
+    {
+        title: "FLOOD FACTORS ANALYTICS AND PREDICTION",
+        brief: "Conducting EDA on a flood prediction dataset, leveraging statistical techniques to enhance the understanding of data features and their relationships to flood risk. Then implementing an XGBoost model to predict flood risk.",
+        tools: ["Python"],
+        link: "https://github.com/Brian2703/Flood_Prediction",
+        image: "images/projects/FLOOD FACTORS ANALYTICS AND PREDICTION.png"
+    },
+    {
+        title: "House Pricing Analysis And Prediction",
+        brief: "Conducting in-depth exploratory data analysis (EDA) on the NSW house price dataset to analyze the trends and how variables like distance from central business districts (CBDs), local amenities, and suburb income levels affected homebuyer decisions and property values. Then, developing machine learning models to predict real estate price trends.",
+        tools: ["Python"],
+        link: "https://github.com/Brian2703/House_Analysis_And_Prediction",
+        image: "images/projects/HOUSING PRICE ANALYTICS AND PREDICTION.png"
+    },
+    {
+        title: "E-Commerce Data Analytics",
+        brief: "This project focuses on developing complex SQL queries to analyze sales, discounts, and profitability in an organization. It examines sales performance across product categories, customer segments, and employee contributions.",
+        tools: ["SQL"],
+        link: "https://github.com/Brian2703/Sales_SQL_Analysis",
+        image: "images/projects/E-Commerce Data Analytics.png"
+    },
+    {
+        title: "Queensland Road Incidents Analytics",
+        brief: "Analyzing factors contributing to road incidents across Queensland to identify critical areas for safety improvements, visualizing the data using multi-type charts in Tableau.",
+        tools: ["Tableau"],
+        link: "https://public.tableau.com/app/profile/brian.ong1771/viz/Story_17272414319140/StoryTellingaboutQueenslandIncidentsdata?publish=yes",
+        image: "images/projects/Queensland Road Incidents Analytics.png"
+    },
+    {
+        title: "Retail Sales Analysis",
+        brief: "Conducting a cohort analysis to assess customer retention and comeback rates to develop Power BI dashboards to track revenue, profit, and sales trends over time.",
+        tools: ["SQL", "Power BI"],
+        link: "https://github.com/Brian2703/Retail_Sales_Analysis",
+        image: "images/projects/Global Electronics Retailer Analysis.png"
+    },
+    {
+        title: "PwC Call Center & Customer Churn Analytics",
+        brief: "Analyzing call center performance and customer churn to provide actionable insights for improving customer retention strategies and enhancing call center operations.",
+        tools: ["Power BI"],
+        link: "https://github.com/Brian2703/PwC_Call_Centre_-_Customer_Churn",
+        image: "images/projects/PwC Call Center & Customer Churn Analytics.png"
+    },
+    {
+        title: "Spam Email Classification",
+        brief: "Implemented multiple models to classify spam email with accuracy over 80% for every model. Compared their performance to select the best model.",
+        tools: ["Python"],
+        link: "https://github.com/Brian2703/Spam_email_classification",
+        image: "images/projects/Spam Email Classification.png"
+    },
+    {
+        title: "Global Electronics Retailer Customer Analysis",
+        brief: "This project focuses on building a Retention Dashboard and Churn Dashboard to analyze customer behavior, retention trends, and churn patterns using SQL and Power BI. Key metrics include Customer Retention Rate (CRR), Repeat Purchase Rate (RPR), Churn Rate, and Cohort Analysis, leveraging structured data from transactions, customer engagement, and feedback. The insights derived will help in identifying at-risk customers, optimizing retention strategies, and improving overall business performance.",
+        tools: ["SQL","Power BI"],
+        link: "https://github.com/Brian2703/Global_Electronic_Retailer_Customer_Analysis",
+        image: "images/projects/Global Electronics Retailer Customer Analysis.png"
+    },
+    {
+        title: "Flower Classification",
+        brief: "This project focuses on flower classification using machine learning to identify different flower species based on visual and morphological features. It involves training models like CNNs (Convolutional Neural Networks) or traditional classifiers (SVM, Random Forest, etc.) on datasets such as Iris Flowers datasets. The goal is to achieve high accuracy in flower recognition, enabling applications in botany, agriculture, and automated plant identification systems.",
+        tools: ["Python"],
+        link: "https://github.com/Brian2703/Flower_classification",
+        image: "images/projects/Flower Classification.png"
+    }
+];
